49da9438cdf861c443cadc1deac492e233da0ed3
Sp. Karagiannis <spyroskar90@gmail.com>
2025-05-26 22:35:10 +0300

    feat: new components created

---
+++ src/components/ArrowFunctionalComponent.tsx
@@ -0,0 +1,6 @@
+const ArrowFunctionalComponent = ()=>{
+const title: string = "Is an Arrow Functional Component";
+return <h1 className="text-center mt-12 text-xl font-bold">{title}</h1>
+}
+
+export default ArrowFunctionalComponent;
