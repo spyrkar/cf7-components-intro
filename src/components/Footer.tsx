c7cc910025e401439a7a43aa1b305ac9b2deff67
Sp. Karagiannis <spyroskar90@gmail.com>
2025-06-03 17:05:20 +0300

    feat: added layout components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer =() => {
+    const currentYear: number = new Date().getFullYear();
+    return (
+        <>
+            <footer className="bg-gray-700">
+                <div className="text-white text-center py-4">
+                    Copyright © {currentYear}, Coding Factory. All rights reserved.
+                </div>
+            </footer>
+
+        </>
+
+    )
+
+}
+
+export default Footer;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./Header.tsx";
+import Footer from "./Footer";
+
+    interface LayoutProps {
+    children: React.ReactNode;
+}
+
+const Layout = ({children}:LayoutProps) => {
+    return (
+        <>
+            <Header/>
+            <div className="containe mx-auto min-h-[95vh] pt-24">
+             {children}
+            </div>
+            <Footer/>
+        </>
+
+    )
+}
+
+export default Layout;
