86ea6e53fc14435c74b3b9a565a2112522d017e8
Sp. Karagiannis <spyroskar90@gmail.com>
2025-06-03 17:05:51 +0300

    feat: using states in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+// import ClassComponent from "./components/ClassComponent.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
+
+
+import Layout from "./components/Layout.tsx";
+import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
+
 
 function App() {
 
   return (
     <>
-    <ClassComponent/>
-      <FunctionalComponent/>
-      <ArrowFunctionalComponent/>
-      <ArrowFunctionalComponentWithProps title="Is an Arrow Functional Component With Props!" />
-      <ArrowFunctionalComponentWithPropsType title="Is an Arrow Functional Component With Props!"
-                                             description="this is a description"/>
+      <Layout>
+    {/*<ClassComponent/>*/}
+    {/*  <ArrowFunctionalComponent/>*/}
+    {/*  <ArrowFunctionalComponentWithProps title="Is an Arrow Functional Component With Props!" />*/}
+    {/*  <ArrowFunctionalComponentWithPropsType title="Is an Arrow Functional Component With Props!"*/}
+    {/*                                         description="this is a description"/>*/}
 
+
+    {/*  <h1 className="text-center text-2xl font-bold">This is a heading1 title</h1>*/}
+    {/*  <FunctionalComponent/>*/}
+
+      {/*<ClassComponentWithState/>*/}
+
+       <FunctionalComponentWithState/>
+
+    </Layout>
     </>
   )
 }
